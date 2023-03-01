4f02f22add92edc57b2437dc973af48c0de0f4ff
Shweta Kapoor <119365944+Shweta-Kapoor-Crafty-Apes-VFX@users.noreply.github.com>
2023-03-01 15:50:18 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+// Import Firebase Functions and the Express app instance
 const functions = require("firebase-functions");
 const app = require("./app");
 
+// Export a Firebase HTTPS function that serves the Express app
 exports.app = functions.https.onRequest(app);
